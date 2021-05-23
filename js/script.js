3f71528ac83c0144d503fc69facc340a8a7413f2
monkeclicker <83822524+monkeclicker@users.noreply.github.com>
2021-05-23 13:24:22 -0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,84 @@
+var nameInput = document.getElementById("nameInput").value
+var entirePage = document.getElementById("entirePage")
+
+if (!localStorage.monkes) {
+    var monkes = 0
+} else if (localStorage.monkes) {
+    var monkes = Number(localStorage.monkes)
+}
+
+if (!localStorage.adder) {
+    var adder = 1
+    localStorage.setItem('adder', adder)
+    var adder = Number(localStorage.adder)
+} else if (localStorage.adder) {
+    localStorage.getItem('adder')
+    var adder = Number(localStorage.adder)
+}
+
+
+var result = document.getElementById("result")
+document.getElementById("disabled").disabled = false;
+
+
+
+function monkeyPressed() {
+    parseInt(localStorage.monkes)
+    monkes = monkes + adder
+    localStorage.setItem('monkes', monkes)
+
+    if (monkes < 1000) {
+        result.innerHTML = "There are " + monkes + " monkes"
+    } else if (monkes >= 1000 && monkes < 1000000) {
+        result.innerHTML = "There are " + (monkes / 1000).toFixed(2) + "K monkes"
+    } else if (monkes >= 1000000 && monkes < 1000000000) {
+        result.innerHTML = "There are " + (monkes / 1000000).toFixed(2) + "M monkes"
+    }
+}
+
+function monkeyPressed2() {
+    if (monkes >= 10) {
+        monkes = monkes - 10
+        result.innerHTML = "There are " + monkes + " monkes"
+        localStorage.setItem('adder', '2')
+        localStorage.setItem('disabled', 'true')
+    }
+
+    if (localStorage.getItem('disabled') === 'true') {
+        document.getElementById("disabled").disabled = true
+    }
+}
+
+
+
+  function enterGame () {
+    var nameInput = document.getElementById("nameInput").value
+    var everything = document.getElementById("everything")
+
+    if (entirePage.style.display === "none" && nameInput.length != 0) {
+        entirePage.style.display = "flex"
+        everything.style.display = "none"
+    }else if (nameInput.length == 0) {
+        console.log('test')
+        var z = "Anonymous"
+        localStorage.setItem('name', z)
+    } 
+};
+
+
+window.onload = function () {
+
+    
+        if (localStorage.getItem('disabled') === 'true'){
+            document.getElementById("disabled").disabled = true;
+        }
+
+    if (monkes < 1000) {
+        result.innerHTML = "There are " + monkes + " monkes"
+    } else if (monkes >= 1000 && monkes < 1000000) {
+        result.innerHTML = "There are " + (monkes / 1000).toFixed(2) + "K monkes"
+    } else if (monkes >= 1000000 && monkes < 1000000000) {
+        result.innerHTML = "There are " + (monkes / 1000000).toFixed(2) + "M monkes"
+    }
+}
+
