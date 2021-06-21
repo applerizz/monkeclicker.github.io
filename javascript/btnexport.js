5d9890a9475c4aa0e3523a48302c15ca7f23d7aa
monkeclicker <83822524+monkeclicker@users.noreply.github.com>
2021-06-20 20:23:24 -0400

    Add files via upload

--- javascript/btnexport.js
+++ javascript/btnexport.js
@@ -1,26 +1,28 @@
-export default class btn {
-    constructor(buttonValue, adder) {
+class btn {
+    constructor(buttonValue, adder, cursorAmount, farmerAmount, kWorker) {
         this.buttonValue = buttonValue
         this.adder = adder
+        this.cursorAmount = cursorAmount
+        this.farmerAmount = farmerAmount
+        this.kWorker = kWorker
     }
 
     cursorCount() {
         const cursorCount = document.getElementById("cursorCount")
-        cursorCount.innerHTML = btn1.buttonValue
+        cursorCount.innerHTML = "Cursor Count: " + btn1.buttonValue.toLocaleString()
+        document.title = btn1.buttonValue.toLocaleString() + " " + "Cursors"
     }
 
+ 
     btnPressed() {
         btn1.buttonValue = btn1.buttonValue += btn1.adder;
-        const cursorCount = document.getElementById("cursorCount")
-        cursorCount.innerHTML = btn1.buttonValue
+        btn1.cursorCount()
     }
 
     upgrade1() {
         if (btn1.buttonValue >= 10) {
             btn1.buttonValue = btn1.buttonValue - 10
             btn1.cursorCount()
-
-            document.getElementById('upgrade1').disabled = true;
 
             let node = document.getElementById("upgrade1");
             if (node.parentNode) {
@@ -38,8 +40,6 @@ export default class btn {
             btn1.buttonValue = btn1.buttonValue - 500
             btn1.cursorCount()
 
-            document.getElementById('upgrade2').disabled = true;
-
             let node = document.getElementById("upgrade2");
             if (node.parentNode) {
                 node.parentNode.removeChild(node);
@@ -50,6 +50,67 @@ export default class btn {
             alert(`You do not have enough cursors! You need ${500-btn1.buttonValue} more!`)
         }
     }
+
+    upgrade3() {
+        if (btn1.buttonValue >= 2500) {
+            btn1.buttonValue = btn1.buttonValue - 2500
+            btn1.cursorCount()
+
+            let node = document.getElementById("upgrade3");
+            if (node.parentNode) {
+                node.parentNode.removeChild(node);
+            }
+
+            btn1.adder = 8
+        } else if (btn1.buttonValue < 2500) {
+            alert(`You do not have enough cursors! You need ${2500-btn1.buttonValue} more!`)
+        }
+    }
+
+    autoclicker1() {
+        var autoClickerCost = Math.floor(50 * Math.pow(1.1, btn1.cursorAmount))
+        if (btn1.buttonValue >= autoClickerCost) {
+            btn1.cursorAmount = btn1.cursorAmount + 1
+            btn1.buttonValue = btn1.buttonValue - autoClickerCost
+
+            var autoClickerCost = Math.floor(50 * Math.pow(1.1, btn1.cursorAmount))
+
+
+            window.setInterval(function () {
+                btn1.btnPressed(btn1.cursorAmount);
+
+            }, 1000);
+        } else if (btn1.buttonValue < autoClickerCost) {
+            return
+        }
+        document.getElementById("autoclickerAmount1").innerHTML = btn1.cursorAmount
+        document.getElementById("autoclickerCost1").innerHTML = autoClickerCost
+        btn1.cursorCount()
+    }
+
+    autoclicker2() {
+        var farmerCost = Math.floor(250 * Math.pow(1.09, btn1.farmerAmount))
+        if (btn1.buttonValue >= farmerCost) {
+            btn1.cursorAmount = btn1.cursorAmount + 1
+            btn1.buttonValue = btn1.buttonValue - farmerCost
+
+            var farmerCost = Math.floor(250 * Math.pow(1.09, btn1.cursorAmount))
+
+
+            window.setInterval(function () {
+                btn1.btnPressed(btn1.cursorAmount);
+
+            }, 500);
+        } else if (btn1.buttonValue < farmerCost) {
+            return
+        }
+        document.getElementById("farmerAmount1").innerHTML = btn1.cursorAmount
+        document.getElementById("farmerCost1").innerHTML = farmerCost
+        btn1.cursorCount()
+    }
 }
 
-let btn1 = new btn(0, 1)
+
+let btn1 = new btn(0, 1, 0, 0, 0)
+
+export default btn
--- javascript/main.js
+++ javascript/main.js
@@ -5,21 +5,35 @@ async function getGameName() {
     const gameName = await response.json()
     const clickName = gameName.gameName
 
-    document.getElementById("myGame").innerHTML = "&nbsp;" + clickName
+    document.getElementById("myGame").innerHTML = "Welcome To: " + clickName
 }
 getGameName()
 
 
-let btn1 = new btn(0, 1)
+
+let btn1 = new btn(0, 1, 0, 0)
 document.getElementById("cursorImage").addEventListener('click', btn1.btnPressed)
+//upgrades
 document.getElementById("upgrade1").addEventListener('click', btn1.upgrade1)
 document.getElementById("upgrade2").addEventListener('click', btn1.upgrade2)
+document.getElementById("upgrade3").addEventListener('click', btn1.upgrade3)
+//autoclickers
+document.getElementById("autoclicker1").addEventListener('click', btn1.autoclicker1)
+document.getElementById("autoclicker2").addEventListener('click', btn1.autoclicker2)
 
 
 window.onload = function () {
     if (btn1.buttonValue == 0) {
         btn1.cursorCount()
     }
+
+    document.getElementById("autoclickerAmount1").innerHTML = btn1.cursorAmount
+    document.getElementById("autoclickerCost1").innerHTML = 50
+
+    document.getElementById("farmerAmount1").innerHTML = btn1.cursorAmount
+    document.getElementById("farmerCost1").innerHTML = 250
+
+    document.getElementById("cursorCount").innerHTML = "Cursor Count: " + btn1.buttonValue
 
     console.log('%cI Have Loaded!', 'font-size: 40px; color: red');
 }
